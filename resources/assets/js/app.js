fbee8eca80f36692c72fa810b512966deba2afec
jidemusty <babajidemusty@gmail.com>
2018-03-13 11:57:06 +0100

    setup vue component

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -15,7 +15,7 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('message-component', require('./components/MessageComponent.vue'));
 
 const app = new Vue({
     el: '#app'
---
+++ resources/assets/js/components/MessageComponent.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card card-default">
+                    <div class="card-header">Message</div>
+
+                    <div class="card-body">
+                        <form action="" @submit.prevent="send">
+                            <div class="form-group">
+                                <label for="users">Choose users</label>
+                                <input type="text" name="users" id="users" class="form-control">
+                            </div>
+
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-success">Send</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            send () {
+                console.log('send message')
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
